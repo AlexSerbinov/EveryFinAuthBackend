08f6674b0dc06460f0964947a060420568838107
Alex.serbinov.dci@gmail.com <Alex.serbinov.dci@gmail.com>
2023-04-17 12:07:58 +0300

    token separate message

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -6,15 +6,76 @@ const { auth } = require('../middlewares/auth');
 
 const Password = require('../controllers/password');
 const validate = require('../middlewares/validate');
-
+/**
+ * @swagger
+ * /api-user/auth:
+ *   get:
+ *     summary: Test endpoint
+ *     responses:
+ *       200:
+ *         description: Returns a test message
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ */
 router.get('/', (req, res) => {
 	console.log(req.headers.host + req.originalUrl);
 	res.status(200).json({
 		message: 'working',
 	});
 });
+/**
+ * @swagger
+ * /api-user/auth/register:
+ *   post:
+ *     summary: Register a new user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 format: email
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: User registered successfully
+ *       400:
+ *         description: Bad request (e.g., invalid input data)
+ */
 
 router.post('/register', validate, Auth.register);
+/**
+ * @swagger
+ * /api-user/auth/login:
+ *   post:
+ *     summary: Log in a user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 format: email
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: User logged in successfully
+ *       400:
+ *         description: Bad request (e.g., invalid input data)
+ */
 
 router.post('/login', Auth.login);
 
